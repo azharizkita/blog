7ca6be77e0a303f303932b1ff7220e1243235e22
azharizkita <azharizkita@icloud.com>
2024-09-11 22:40:45 +0700

    chore: add analytics
    Signed-off-by: azharizkita <azharizkita@icloud.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import styles from "./layout.module.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const font = Chakra_Petch({
   subsets: ["latin"],
@@ -31,6 +32,7 @@ export default function RootLayout({
           <Footer />
         </div>
         <SpeedInsights sampleRate={0.5} />
+        <Analytics  />
       </body>
     </html>
   );
