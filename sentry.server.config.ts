fa18c2a140051398bb02dcb398dacfda618ff2ea
azharizkita <azharizkita@icloud.com>
2025-01-29 17:14:39 +0700

    chore: add back sentry
    Signed-off-by: azharizkita <azharizkita@icloud.com>

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import { init } from "@sentry/nextjs";
+
+init({
+  dsn: "https://c00e70cc41d2ad8e38bed054c34518b1@o4507924468924416.ingest.us.sentry.io/4507934543052800",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
