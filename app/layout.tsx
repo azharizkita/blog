f7244f6b4c9bce453e742f35b5dd68a8a9ac239b
azharizkita <azharizkita@icloud.com>
2025-05-04 22:13:25 +0700

    chore: improve codes
    Signed-off-by: azharizkita <azharizkita@icloud.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
     >
       <body className="flex flex-1 flex-col w-full">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="w-full max-w-2xl px-6 pb-6 flex flex-col mx-auto h-full">
+          <div className="w-full max-w-2xl px-6 pb-6 gap-4 flex flex-col mx-auto h-full">
             <header className="z-30 sticky top-0 flex flex-col shrink-0 w-full justify-center">
               <div className="dark:bg-black bg-white pt-3 flex flex-col gap-2">
                 <HeadBar />
--- app/who-am-i/page.tsx
+++ app/who-am-i/page.tsx
@@ -1,5 +1,23 @@
-import About from "@/components/about";
+import ArticleContent from "@/components/article-content";
+import { getAbout } from "@/repositories/about";
+import type { Metadata } from "next";
 
-export default function Me() {
-  return <About />;
+export const metadata: Metadata = {
+  metadataBase: new URL("https://silenced.life"),
+  title: "Silenced | Who am I",
+  description:
+    "A human—just like you. I tell stories. Sometimes with words, sometimes with metaphors, but always from the heart. And I code too.",
+  openGraph: {
+    url: "https://silenced.life/who-am-i",
+    siteName: "Silenced | Stats",
+    images: [{ url: "/api/og?title=Who am I" }],
+  },
+};
+
+export default async function WhoAmI() {
+  const content = await getAbout();
+
+  if (!content) return null;
+
+  return <ArticleContent content={content} />;
 }
