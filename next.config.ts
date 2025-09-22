730dd212ec4dbb17e9347972dba61cce032ecc08
Azhari Rizkita <azharizkita@icloud.com>
2025-09-23 02:31:06 +0700

    fix: sentry project name

--- next.config.ts
+++ next.config.ts
@@ -40,7 +40,7 @@ export default withSentryConfig(withSerwist(nextConfig), {
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
   org: "hexalts",
-  project: "lokey-bio",
+  project: "silenced-life",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
