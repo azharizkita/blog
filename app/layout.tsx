1163d9539be4d859de176c7d8fd5ed9a0ef302b7
Azhari Rizkita <azharizkita@icloud.com>
2026-01-04 11:06:46 +0700

    feat: ux improvement

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,19 +43,18 @@ export default function RootLayout({
       className={`${inter.variable} ${roboto_mono.variable} antialiased`}
       suppressHydrationWarning
     >
-      <body className="flex flex-1 flex-col w-full pt-32">
+      <body className="flex flex-1 flex-col w-full">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <NavigationProvider>
-            <header className="z-30 fixed top-0 left-0 right-0 flex flex-col w-full">
+            <header className="z-30 sticky top-0 left-0 right-0 flex flex-col w-full">
               <div className="dark:bg-black bg-white w-full">
                 <div className="w-full max-w-2xl mx-auto px-6 pt-6 flex flex-col gap-3 pb-1">
                   <HeadBar />
-                  <CategoryTab />
                 </div>
               </div>
               <div className="w-full h-8 bg-gradient-to-b from-white via-white/60 via-40% to-white/0 dark:from-black dark:via-black/60 dark:via-40% dark:to-black/0" />
             </header>
-            <div className="w-full max-w-2xl px-6 p-6 gap-3 flex flex-col mx-auto h-full">
+            <div className="w-full max-w-2xl px-6 pb-6 gap-3 flex flex-col mx-auto h-full">
               {children}
             </div>
           </NavigationProvider>
